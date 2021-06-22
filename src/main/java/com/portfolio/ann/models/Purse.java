e834bfe2e37524f850a54c13315d7aecb5bf86f1
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-06-22 13:57:12 -0500

    models getters and setters, constructors

--- src/main/java/com/portfolio/ann/models/Purse.java
+++ src/main/java/com/portfolio/ann/models/Purse.java
@@ -1,4 +1,52 @@
 package com.portfolio.ann.models;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="dresses")
 public class Purse {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        @Column(nullable = false)
+        private String title;
+
+        @Column(columnDefinition = "TEXT NOT NULL")
+        private String description;
+
+        public Purse() {
+        }
+
+        public Purse(long id, String title, String description) {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
 }
