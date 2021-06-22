8b2f06618c733d2eed9f8f14c7f68576035a6929
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-06-21 21:00:20 -0500

    spring/tomcat config

---
+++ src/main/java/com/portfolio/ann/AnnDev.java
@@ -0,0 +1,13 @@
+package com.portfolio.ann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnDev {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnDev.class, args);
+    }
+
+}
---
+++ src/main/java/com/portfolio/ann/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.portfolio.ann.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String welcome() {
+        return "home";
+    }
+}
