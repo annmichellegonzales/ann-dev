4e806b67c7123e7a9a13dd1c7628520998f44cee
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-06-28 14:44:48 -0500

    spellwork page

--- src/main/java/com/portfolio/ann/controllers/SpellWorkController.java
+++ src/main/java/com/portfolio/ann/controllers/SpellWorkController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ForecastController {
-    @GetMapping("/forecast")
-    public String showForecast() {
-        return "forecast";
-    }
+public class SpellWorkController {
+    @GetMapping("/spellwork")
+    public String showSpell() {return "spellwork";
+    };
+
 }
