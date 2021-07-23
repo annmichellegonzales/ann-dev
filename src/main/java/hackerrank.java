eacb836c9ffcb2ab3c290c002a867600dd11290d
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-07-23 16:09:16 -0500

    numbered strings, hackerrank

--- src/main/java/hackerrank.java
+++ src/main/java/hackerrank.java
@@ -93,4 +93,22 @@ public class hackerrank {
 
 //    String s = Integer.toString(n);
 
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        for(int i = 1; scan.hasNext()== true; i++){
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
+
+// // While loop System
+// public static void main(String[] args) {
+//    Scanner scan = new Scanner(System.in);
+//    int i = 0;
+//    while(scan.hasNext()){
+//        i++;
+//        System.out.println(i + " " + scan.nextLine());
+//    }
+//     }
+
 }
