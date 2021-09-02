55646922a7c3fd87bcbbb08fde077477b84231fd
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-09-02 12:04:41 -0500

    java exercise

---
+++ src/main/java/com/portfolio/ann/DooBeeDo.java
@@ -0,0 +1,15 @@
+package com.portfolio.ann;
+
+public class DooBeeDo {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 4) {
+            System.out.println("Doo");
+            System.out.println("Bee");
+            x = x + 1;
+        }
+        if (x == 4) {
+            System.out.println("Do");
+        }
+    }
+}
