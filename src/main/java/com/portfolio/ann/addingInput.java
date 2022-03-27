f50de83b3e641d953dd7225cbab82144f21b9c23
annmichellegonzales <ann.michelle.gonzales112@gmail.com>
2022-03-27 14:21:45 -0500

    feat: scanner input

---
+++ src/main/java/com/portfolio/ann/addingInput.java
@@ -0,0 +1,15 @@
+package com.portfolio.ann;
+
+import java.util.Scanner;
+
+public class addingInput {
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username");
+
+        String userName =  myObj.nextLine();
+        System.out.println("Username is: " + userName);
+    }
+    
+}
--- src/main/java/com/portfolio/ann/remove_duplicates.java
+++ src/main/java/com/portfolio/ann/remove_duplicates.java
@@ -9,21 +9,40 @@ public class remove_duplicates {
 // uses for loop to iterate over the integers
 // conditional to check if element is in HashMap
 
-    static void removeDups(int[] a, int n){
+    // static void removeDups(int[] a, int n){
         
+    //     HashMap<Integer, Boolean> mp = new HashMap<>();
+
+    //     for(int i=0; i<n; i++){
+    //         if (mp.get(a[i]) == null){
+    //             System.out.println(a[i] + " ");
+    //             mp.put(a[i], true);
+    //         }
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     int [] arr = {1,1,2,2,2};
+    //     int n = arr.length;
+    //     removeDups(arr, n);
+    // }
+
+
+    static void removeDups(int[] arr){
+
         HashMap<Integer, Boolean> mp = new HashMap<>();
 
-        for(int i=0; i<n; i++){
-            if (mp.get(a[i]) == null){
-                System.out.println(a[i] + " ");
-                mp.put(a[i], true);
+        for(int i=0; i<arr.length; i++){
+            if(mp.get(arr[i]) == null){
+                System.out.println(arr[i] + " ");
+                mp.put(arr[i], true);
             }
         }
+
     }
     public static void main(String[] args) {
-        int [] arr = {1,1,2,2,2};
-        int n = arr.length;
-        removeDups(arr, n);
+        int [] arr = {1,2,3,3,4,5};
+        removeDups(arr);
+        
     }
 
 }
