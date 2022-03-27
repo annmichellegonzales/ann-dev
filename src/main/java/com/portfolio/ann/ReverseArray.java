6c3b02bbc73ebb2215c14c4e1febb1dd7ce41ee5
annmichellegonzales <ann.michelle.gonzales112@gmail.com>
2022-03-27 15:43:36 -0500

    feat: palindrome

--- src/main/java/com/portfolio/ann/ReverseArray.java
+++ src/main/java/com/portfolio/ann/ReverseArray.java
@@ -1,5 +1,7 @@
 package com.portfolio.ann;
 
+import java.util.Scanner;
+
 public class ReverseArray {
 
     // public static void main(String[] args) {
@@ -16,18 +18,48 @@ public class ReverseArray {
 
     // }
 
-    public static void main(String[] args) {
-        int[] arr1 = new int[]{1,2,3,4,5};
-        System.out.println("The original array: ");
-        for(int i=0; i<arr1.length; i++){
-            System.out.print(arr1[i] + " ");
-        }
-        System.out.println();
-        System.out.println("The reversed array: ");
-        for(int i=arr1.length-1; i>=0; i--){
-            System.out.print(arr1[i] + " ");
-        }
+    // public static void main(String[] args) {
+    //     int[] arr1 = new int[]{1,2,3,4,5};
+    //     System.out.println("The original array: ");
+    //     for(int i=0; i<arr1.length; i++){
+    //         System.out.print(arr1[i] + " ");
+    //     }
+    //     System.out.println();
+    //     System.out.println("The reversed array: ");
+    //     for(int i=arr1.length-1; i>=0; i--){
+    //         System.out.print(arr1[i] + " ");
+    //     }
 
+    // }
+
+    // public static void main(String[] args) {
+    //     int[] arr1 = {1,2,454,5,5,2};
+    //     for(int i =  arr1.length-1; i>=0; i--){
+    //         System.out.println(arr1[i] + " ");
+    //     }
+    // }
+
+
+    // public static void main(String[] args) {
+    //     int[] arr1 = {1, 3, 5, 3, 2};
+
+    //     for(int i= arr1.length-1; i>=0; i--){
+    //         System.out.println(arr1[i]);
+    //     }
+    // }
+
+    // public static void main(String[] args) {
+    //     int[] arr1 = {2, 1, 4, 55, 3, 24};
+    //     for(int i=arr1.length-1; i>=0; i--){
+    //         System.out.println(arr1[i]);
+    //     }
+    // }
+
+    public static void main(String[] args) {
+        int [] arr1 = {32,4,5,42,3,23,3,443};
+        for(int i=arr1.length-1; i>=0; i--){
+            System.out.println(arr1[i]);
+        }
     }
 
     
--- src/main/java/com/portfolio/ann/addingInput.java
+++ src/main/java/com/portfolio/ann/addingInput.java
@@ -2,15 +2,73 @@ package com.portfolio.ann;
 
 import java.util.Scanner;
 
+import org.springframework.util.SystemPropertyUtils;
+
 public class addingInput {
 
-    public static void main(String[] args) {
-        try (Scanner myObj = new Scanner(System.in)) {
-            System.out.println("Enter username");
+    // public static void main(String[] args) {
+    //     try (Scanner myObj = new Scanner(System.in)) {
+    //         System.out.println("Enter username:");
 
-            String userName =  myObj.nextLine();
-            System.out.println("Username is: " + userName);
-        }
-    }
+    //         String userName =  myObj.nextLine();
+    //         System.out.println("Username is: " + userName);
+    //     }
+    // }
+
+
+    // public static void main(String[] args) {
+    //     try (Scanner userInput = new Scanner(System.in)) {
+    //         System.out.println("Please enter your age: ");
+
+    //         String userAge = userInput.nextLine();
+    //         System.out.println("You are " + userAge + " years old.");
+    //     }
+    // }
     
+
+    // public static void main(String[] args) {
+    //     try (Scanner userInput = new Scanner(System.in)) {
+    //         System.out.println("Where are you from?");
+
+    //         String userLocation = userInput.nextLine();
+    //         System.out.println("You live in: " + userLocation);
+    //     }
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner userInput = new Scanner(System.in);
+    //     System.out.println("What's your favorite food?");
+
+    //     String favFood = userInput.nextLine();
+    //     System.out.println("Mmm " + favFood.toLowerCase() + " sounds delicious!");
+    // }
+
+
+    // public static void main(String[] args) {
+    //     Scanner userInput = new Scanner(System.in);
+    //     System.out.println("Who do you have a crush on?");
+
+    //     String userCrush = userInput.next();
+    //     System.out.println("Tell " + userCrush + " how you feel.");
+    // }
+
+
+    // public static void main(String[] args) {
+    //     try (Scanner userInput = new Scanner(System.in)) {
+    //         System.out.println("Who do you have a crush on?");
+
+    //         String userCrush = userInput.next();
+    //         System.err.println("OOOH! I didn't know you have a crush on " + userCrush + " !");
+    //     }
+    // }
+
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("What's your favorite toy?");
+
+        String userToy = userInput.next();
+        System.out.println("DING! Your favorite toy is: " + userToy);
+    }
+
 }
